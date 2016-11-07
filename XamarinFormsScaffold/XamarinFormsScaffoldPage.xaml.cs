29819f9e13300f03e44fd474926fdfca1b33f859
Guilherme Ikeda <Guilherme Ikeda>
2016-11-07 21:03:21 -0200

    First Commit

---
+++ XamarinFormsScaffold/XamarinFormsScaffoldPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinFormsScaffold
+{
+	public partial class XamarinFormsScaffoldPage : ContentPage
+	{
+		public XamarinFormsScaffoldPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
